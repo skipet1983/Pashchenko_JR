e8513e86f3f1c74ade228c81ca05529e64d62c3e
Anton Pashchenko <anton@blokc.no>
2024-11-22 11:08:46 +0100

    Commit Pashchenko_A module

--- Pashchenko_A/src/main/java/ua/com/javarush/gnew/model/map/Cell.java
+++ Pashchenko_A/src/main/java/ua/com/javarush/gnew/model/map/Cell.java
@@ -1,4 +1,26 @@
 package ua.com.javarush.gnew.model.map;
 
+import ua.com.javarush.gnew.model.organizm.Organizm;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Cell {
+
+    private final Map<Class<? extends Organizm>, Set<Organizm>> residents;
+
+    public Cell(Map<Class<? extends Organizm>, Set<Organizm>> residents) {
+        this.residents = residents;
+    }
+
+    public Map<Class<? extends Organizm>, Set<Organizm>> getResidents() {
+        return residents;
+    }
+
+    public boolean add (Organizm organizm){
+        Class<? extends Organizm> organizmClass = organizm.getClass();
+        residents.putIfAbsent(organizmClass, new HashSet<>());
+        return residents.get(organizmClass).add(organizm);
+    }
 }
